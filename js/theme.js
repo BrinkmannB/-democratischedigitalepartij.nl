644e3daadd08cd2b5ea589b02bed4ced16775d2f
Bas Brinkmann <brinkmann.bas@icloud.com>
2025-11-30 16:15:06 +0100

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,7 @@
+const menuBtn = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector("nav ul");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
 

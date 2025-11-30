91256e9c08f2439399220899645db712c9237416
Bas Brinkmann <brinkmann.bas@icloud.com>
2025-11-30 16:26:04 +0100

    Update PWA service worker — sw.js

--- PWA service worker — sw.js
+++ PWA service worker — sw.js
@@ -1,1 +1,8 @@
+self.addEventListener("install", (event) => {
+  console.log("Service worker installed");
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service worker active");
+});
 
